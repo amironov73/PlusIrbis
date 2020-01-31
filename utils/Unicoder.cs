f5c6d60e15f2ec45ee582a1415d702244237a85d
Alexey Mironov <amironov73@gmail.com>
2020-01-31 16:17:04 +0800

    Convert characters to Unicode escapes

---
+++ utils/Unicoder.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            ProcessString(arg);
+        }
+    }
+
+    private static void ProcessString(string s)
+    {
+        foreach (var c in s)
+        {
+            if (c < 256)
+            {
+                Write(c);
+            }
+            else
+            {
+                Write("\\u{0:X4}", (int)c);
+            }
+        }
+        WriteLine();
+    }
+}
