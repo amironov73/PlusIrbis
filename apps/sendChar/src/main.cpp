373ec88fdc9e979e5c61d9a4ec516816ffa36e9f
Alexey Mironov <amironov73@gmail.com>
2020-02-14 17:29:37 +0800

    Add sendChar application

---
+++ apps/sendChar/src/main.cpp
@@ -0,0 +1,176 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#include <iostream>
+#include "irbis.h"
+#include "rfid.h"
+
+#ifdef IRBIS_WINDOWS
+
+#include <windows.h>
+#pragma comment(lib, "winscard.lib")
+
+static const char CLASS_NAME[] = "SendChar Window Class";
+static const char mainTitle[] = "SendChar by Miron";
+static HWND mainWindow;
+static std::string mainText ("........");
+static const char readerName[] = "OMNIKEY CardMan 5x21-CL 0";
+static irbis::WinSmartCards mgr;
+
+static void sendText (HWND hwnd, const std::string &text)
+{
+    for (const auto c : text) {
+        SendMessageA (hwnd, WM_CHAR, c, 0);
+        Sleep (10);
+    }
+    SendMessageA (hwnd, WM_CHAR, '\r', 0);
+    //SendMessageA (hwnd, WM_CHAR, '\n', 0);
+}
+
+void sendText (const std::string &text)
+{
+    HWND hwnd = GetForegroundWindow();
+    DWORD threadId = GetWindowThreadProcessId (hwnd, nullptr);
+    GUITHREADINFO gui;
+    memset (&gui, 0, sizeof (gui));
+    gui.cbSize = sizeof (gui);
+    GetGUIThreadInfo(threadId, &gui);
+    HWND hcaret = gui.hwndCaret;
+    sendText (hcaret, text);
+}
+
+static void readCard()
+{
+    if (!mgr.connect(readerName)) {
+        // PostQuitMessage(0);
+        return;
+    }
+
+    const auto card = mgr.readCard();
+    if (!card.empty()) {
+        mainText = card;
+        sendText (card);
+        InvalidateRect (mainWindow, nullptr, true);
+    }
+
+    mgr.disconnect();
+}
+
+static void CALLBACK MyTimerProc (HWND Arg1, UINT Arg2, UINT_PTR Arg3, DWORD Arg4)
+{
+    readCard();
+}
+
+static LRESULT CALLBACK MyWindowProc (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint (hwnd, &ps);
+
+            FillRect (hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
+            DrawText (hdc, mainText.data(), static_cast<int>(mainText.size()), &ps.rcPaint,
+                      DT_NOPREFIX|DT_CENTER|DT_VCENTER);
+
+            EndPaint (hwnd, &ps);
+        }
+            return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+static void registerClass (HINSTANCE hInstance)
+{
+    WNDCLASS wc {};
+    wc.lpfnWndProc   = MyWindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hIcon         = LoadIcon (hInstance, "info");
+
+    RegisterClass (&wc);
+}
+
+static void createWindow (HINSTANCE hInstance)
+{
+    DWORD style = WS_CAPTION|WS_SYSMENU;
+    DWORD styleEx = WS_EX_APPWINDOW;
+
+    mainWindow = CreateWindowEx
+            (
+                    styleEx,      // Optional window styles.
+                    CLASS_NAME,   // Window class
+                    mainTitle,    // Window text
+                    style,        // Window style
+
+                    // Position
+                    CW_USEDEFAULT, CW_USEDEFAULT,
+
+                    // Size
+                    250, 65,
+
+                    nullptr,       // Parent window
+                    nullptr,       // Menu
+                    hInstance,     // Instance handle
+                    nullptr        // Additional application data
+            );
+
+}
+
+static void messageLoop()
+{
+    MSG msg {};
+    while (GetMessage (&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE, PSTR pCmdLine, int nCmdShow)
+{
+    if (!irbis::isRfidSupported()) {
+        MessageBox (nullptr, "RFID is not supported on this platform",
+                    "ERROR", MB_OK|MB_ICONSTOP);
+        return 1;
+    }
+
+    if (!mgr.open()) {
+        MessageBox (nullptr, "Can't open RFID manager",
+                    "ERROR", MB_OK|MB_ICONSTOP);
+        return 1;
+    }
+
+    const auto readers = mgr.listReaders();
+    if (readers.empty()) {
+        MessageBox (nullptr, "No readers in the system",
+                    "ERROR", MB_OK|MB_ICONSTOP);
+        return 1;
+    }
+
+    registerClass (hInstance);
+    createWindow (hInstance);
+    ShowWindow (mainWindow, nCmdShow);
+    SetTimer (mainWindow, 0, 1000, MyTimerProc);
+    messageLoop();
+
+    mgr.disconnect();
+    mgr.close();
+
+    return 0;
+}
+
+#else
+
+int main()
+{
+    return 0;
+}
+
+#endif
--- build_mingw.cmd
+++ build_mingw.cmd
@@ -5,6 +5,7 @@ mkdir artifacts > nul 2> nul
 
 rem SET MINGW_PATH=C:/Qt/Tools/mingw492_32/bin
 SET MINGW_PATH=C:/Qt/Tools/mingw530_32/bin
+SET MINGW_PATH2=C:\Qt\Tools\mingw530_32\bin
 
 SET MINGW_C=%MINGW_PATH%/gcc.exe
 SET MINGW_CPP=%MINGW_PATH%/g++.exe
@@ -15,3 +16,7 @@ SET PATH=%MINGW_PATH%;%PATH%
 %CMAKE% -B ./build -G "MinGW Makefiles" -D CMAKE_C_COMPILER=%MINGW_C% -D CMAKE_CXX_COMPILER=%MINGW_CPP% -D CMAKE_MAKE_PROGRAM=%MINGW_MAKE% -D CMAKE_BUILD_TYPE=Release -D IRBIS_MINGW=1 .
 %CMAKE% --build ./build --config Release -j 2
 %CMAKE% --install ./build --config Release 
+
+copy "%MINGW_PATH2%\libgcc_s_dw2-1.dll"  artifacts
+copy "%MINGW_PATH2%\libstdc++-6.dll"     artifacts
+copy "%MINGW_PATH2%\libwinpthread-1.dll" artifacts
